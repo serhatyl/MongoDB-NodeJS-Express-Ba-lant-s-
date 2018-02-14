d0032d20db3357b11a6c86723ef9d1e91934acc8
Serhat <yilmaz.srhat@gmail.com>
2018-02-14 23:15:02 +0300

    upsert , findByIdAndUpdate

--- routes/books.js
+++ routes/books.js
@@ -1,64 +1,82 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 //Models
-const Book = require('../models/Book');
+const Book = require("../models/Book");
 
-router.post('/new', function (req, res, next) {
-    const book = new Book({
-        title: 'PHP',
-        isPublished: false,
-        comments: [
-            { message: 'Harika bir kitap' }, { message: 'Ben pek beğenmedim' }
-        ],
-        meta: {
-            votes: 4,
-            favs: 10
-        }
-    });
+router.post("/new", function(req, res, next) {
+  const book = new Book({
+    title: "PHP",
+    isPublished: false,
+    comments: [
+      { message: "Harika bir kitap" },
+      { message: "Ben pek beğenmedim" }
+    ],
+    meta: {
+      votes: 4,
+      favs: 10
+    }
+  });
 
-    book.save((err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.json({ data });
-        }
-    });
-
+  book.save((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json({ data });
+    }
+  });
 });
 
 //search route
 //find
-router.get('/search', (req, res) => {
-    Book.find({ isPublished: false, title: 'Da Vinci' }, 'title , comments', (err, data) => {
-        res.json(data);
-    });
+router.get("/search", (req, res) => {
+  Book.find(
+    { isPublished: false, title: "Da Vinci" },
+    "title , comments",
+    (err, data) => {
+      res.json(data);
+    }
+  );
 });
-
 
 //findOne
-router.get('/searchOne', (req, res) => {
-    Book.findOne({ title: 'Da Vinci' }, (err, data) => {
-        res.json(data);
-    });
+router.get("/searchOne", (req, res) => {
+  Book.findOne({ title: "Da Vinci" }, (err, data) => {
+    res.json(data);
+  });
 });
-
 
 //findById
-router.get('/searchById', (req, res) => {
-    Book.findById('5a84401f3cc9353b24a12f62', (err, data) => {
-        res.json(data);
-    });
+router.get("/searchById", (req, res) => {
+  Book.findById("5a84401f3cc9353b24a12f62", (err, data) => {
+    res.json(data);
+  });
 });
-
 
 //güncelleme işlemi birden fazla alan varsa ilk bulduğu kayıdı günceller
 // birden fazla alanı güncellemek isterseniz multi:true eklemek gerekiyor
-router.put('/update', (req, res) => {
-    Book.update({ isPublished: false }, { isPublished: true }, { multi: true }, (err, data) => {
-        res.json(data);
-    });
+router.put("/update", (req, res) => {
+  Book.update(
+    { isPublished: false },
+    { isPublished: true },
+    // { multi: true },
+    { upsert: true }, //upsert update sırasında false olan kayıt bulamazsa gidip yeni bir kayıt ekliyor
+    (err, data) => {
+      res.json(data);
+    }
+  );
 });
 
+router.put("/updateById", (req, res) => {
+  Book.findByIdAndUpdate(
+    "5a84958d2e4d921ad823c243",
+    {
+      title: "NodeJS Kitabı",
+      "meta.favs": 11
+    },
+    (err, data) => {
+      res.json(data);
+    }
+  );
+});
 
 module.exports = router;
